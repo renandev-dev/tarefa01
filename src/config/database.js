08899e564bbc4e2883a2ff691e1951f91c042469
renandev-dev <helberthrenan00@gmail.com>
2026-08-25 00:33:17 +0000

    att

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,9 @@
 // src/config/database.js
-import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
